d1982a488fb3cde8bf7599402236760446e02d00
SFujiN <d0n4lddanghuynh@gmail.com>
2026-09-20 18:35:17 -0500

    Undo

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.BASE_PATH || ''
+			base: process.argv.includes('dev') ? '' : '/my-vis-5609'
 		}
 	}
 };
