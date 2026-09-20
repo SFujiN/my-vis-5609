0198a0e60837781117ed655ef2a4507c999c06e0
SFujiN <d0n4lddanghuynh@gmail.com>
2026-09-20 18:29:58 -0500

    Fix?

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.BASE_PATH || ''
 		}
 	}
 };
