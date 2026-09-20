f0ea4c47e0f95efacf125ee40d5159406fad5244
SFujiN <d0n4lddanghuynh@gmail.com>
2026-09-20 18:51:26 -0500

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,15 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+					pages: 'build',
+					assets: 'build',
+					fallback: '404.html',
+					strict: true
+			}),
+			paths: {
+				base: process.env.BASE_PATH || ''
+			}
 		})
 	]
 });
